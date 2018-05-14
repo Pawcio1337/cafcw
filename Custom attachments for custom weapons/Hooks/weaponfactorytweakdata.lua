6f3e192b9930d8d03ee1fb5cf568a3cbe826ea8b
Pawcio1337 <pawcio1337@gmail.com>
2018-05-14 23:18:17 +0200

    1.251

--- Custom attachments for custom weapons/Hooks/weaponfactorytweakdata.lua
+++ Custom attachments for custom weapons/Hooks/weaponfactorytweakdata.lua
@@ -2586,85 +2586,106 @@ end
 if self.wpn_fps_ass_scar_m203 then
 if self.parts.wpn_fps_upg_o_kobra then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_kobra")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_kobra = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_kobra.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_compm4s then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_compm4s")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_compm4s = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_compm4s.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_m145 then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_m145")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_m145 = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_m145.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_pkas then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_pkas")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_pkas = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_pkas.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_coyote then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_coyote")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_coyote = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_coyote.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_acog_rmr then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_acog_rmr")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_acog_rmr = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_acog_rmr.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_acog.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_hologram then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_hologram")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_hologram = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_hologram.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_gitsch then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_gitsch")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_gitsch = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_gitsch.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_acog.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_zeiss then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_zeiss")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_zeiss = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_zeiss.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_okp7 then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_okp7")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_okp7 = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_okp7.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_hd33 then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_hd33")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_hd33 = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_hd33.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_prismatic then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_prismatic")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_prismatic = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_prismatic.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_fl_ass_spotter then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_fl_ass_spotter")
+	self.parts.wpn_fps_upg_scar_m203_gadgets_leftrail.override.wpn_fps_upg_fl_ass_spotter = {a_obj = "a_fl_leftrail"}
 end
 if self.parts.wpn_fps_upg_o_srs then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_srs")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_srs = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_srs.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_st10 then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_st10")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_st10 = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_st10.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_st10.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_hcog then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_hcog")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_hcog = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_hcog.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_reflexholo then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_reflexholo")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_reflexholo = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_reflexholo.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_aog then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_aog")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_aog = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_aog.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_acog.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_rmr_riser then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_rmr_riser")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_rmr_riser = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_rmr_riser.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_elo then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_elo")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_elo = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_elo.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_po4 then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_po4")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_po4 = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_po4.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_po4.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_ass_ns_g_sup3 and self.parts.wpn_fps_ass_ns_g_sup4 then
@@ -2673,17 +2694,21 @@ table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_ass_ns_g_sup4")
 end
 if self.parts.wpn_fps_upg_fl_wml then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_fl_wml")
+	self.parts.wpn_fps_upg_scar_m203_gadgets_leftrail.override.wpn_fps_upg_fl_wml = {a_obj = "a_fl_leftrail"}
 end
 if self.parts.wpn_fps_upg_o_kemper then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_kemper")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_kemper = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_kemper.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_mepro then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_mepro")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_mepro = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_mepro.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_rusak then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_rusak")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_rusak = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_rusak.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_riflein556 then
@@ -2694,18 +2719,21 @@ table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_a_taser556")
 end
 if self.parts.wpn_fps_upg_o_delta_rm55 then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_delta_rm55")
-	self.parts.wpn_fps_upg_o_delta_rm55.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_45rds.stance_mod.wpn_fps_ass_contraband)
+	self.parts.wpn_fps_upg_o_delta_rm55.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_45rds.stance_mod.wpn_fps_ass_scar_m203)
 end
 if self.parts.wpn_fps_upg_o_horzine then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_horzine")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_horzine = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_horzine.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_susat then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_susat")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_susat = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_susat.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_susat.stance_mod.wpn_fps_ass_contraband)
 end
 if self.parts.wpn_fps_upg_o_eotech552 then
 table.insert(self.wpn_fps_ass_scar_m203.uses_parts, "wpn_fps_upg_o_eotech552")
+	self.wpn_fps_ass_scar_m203.adds.wpn_fps_upg_o_eotech552 = {"wpn_fps_ass_scar_m203_irons_front"}
 	self.parts.wpn_fps_upg_o_eotech552.stance_mod.wpn_fps_ass_scar_m203 = deep_clone(self.parts.wpn_fps_upg_o_eotech552.stance_mod.wpn_fps_ass_contraband)
 end
 end
@@ -7758,6 +7786,33 @@ if self.parts.wpn_fps_upg_o_susat then
 table.insert(self.wpn_fps_smg_pps43.uses_parts, "wpn_fps_upg_o_susat")
 	self.wpn_fps_smg_pps43.adds.wpn_fps_upg_o_susat = {"wpn_fps_smg_pps43_rail"}
 	self.parts.wpn_fps_upg_o_susat.stance_mod.wpn_fps_smg_pps43 = deep_clone(self.parts.wpn_fps_upg_o_susat.stance_mod.wpn_fps_smg_m45)
+end
+end
+-- Chiappa Rhino 60DS
+if self.wpn_fps_pis_rhino then
+if self.parts.wpn_fps_upg_o_compm4s then
+table.insert(self.wpn_fps_pis_rhino.uses_parts, "wpn_fps_upg_o_compm4s")
+table.insert(self.parts.wpn_fps_upg_rhino_frame_200ds.forbids, "wpn_fps_upg_o_compm4s")
+table.insert(self.parts.wpn_fps_upg_rhino_frame_200ds_chrome.forbids, "wpn_fps_upg_o_compm4s")
+	self.parts.wpn_fps_upg_o_compm4s.stance_mod.wpn_fps_pis_rhino = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_pis_rhino)
+	self.wpn_fps_pis_rhino.override.wpn_fps_upg_o_compm4s = {a_obj = "a_o_aimpoint"}
+end
+if self.parts.wpn_fps_upg_o_okp7 then
+table.insert(self.wpn_fps_pis_rhino.uses_parts, "wpn_fps_upg_o_okp7")
+table.insert(self.parts.wpn_fps_upg_rhino_frame_200ds.forbids, "wpn_fps_upg_o_okp7")
+table.insert(self.parts.wpn_fps_upg_rhino_frame_200ds_chrome.forbids, "wpn_fps_upg_o_okp7")
+	self.parts.wpn_fps_upg_o_okp7.stance_mod.wpn_fps_pis_rhino = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_pis_rhino)
+	self.wpn_fps_pis_rhino.override.wpn_fps_upg_o_okp7 = {a_obj = "a_o_aimpoint"}
+end
+if self.parts.wpn_fps_upg_o_rmr_riser then
+table.insert(self.wpn_fps_pis_rhino.uses_parts, "wpn_fps_upg_o_rmr_riser")
+table.insert(self.parts.wpn_fps_upg_rhino_frame_200ds.forbids, "wpn_fps_upg_o_rmr_riser")
+table.insert(self.parts.wpn_fps_upg_rhino_frame_200ds_chrome.forbids, "wpn_fps_upg_o_rmr_riser")
+	self.parts.wpn_fps_upg_o_rmr_riser.stance_mod.wpn_fps_pis_rhino = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_pis_rhino)
+end
+if self.parts.wpn_fps_upg_fl_wml then
+table.insert(self.wpn_fps_pis_rhino.uses_parts, "wpn_fps_upg_fl_wml")
+	self.wpn_fps_pis_rhino.override.wpn_fps_upg_fl_wml = {a_obj = "a_fl_alt"}
 end
 end
 --
