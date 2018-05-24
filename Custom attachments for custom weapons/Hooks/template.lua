bb85cc58d79079445402f5f41a38219179132e85
Pawcio1337 <pawcio1337@gmail.com>
2018-05-24 15:37:32 +0200

    1.260-WIP2

--- Custom attachments for custom weapons/Hooks/template.lua
+++ Custom attachments for custom weapons/Hooks/template.lua
@@ -1,36 +1,79 @@
--- Add your mostly used nick on Modworkshop to ID below (Change only MWSNICK part):
+function WeaponFactoryTweakData:cafcw_add_to_parts(part_type, param1, param2, param3, param4, param5)
+	if self.parts[param2] then
+		if part_type == "forbids" and self.parts[param1] then
+			table.insert(self.parts[param1].forbids, param2)
+			if param3 and self.parts[param3] then
+				table.insert(self.parts[param3].forbids, param2)
+			end
+			if param4 and self.parts[param4] then
+				table.insert(self.parts[param4].forbids, param2)
+			end
+			if param5 and self.parts[param5] then
+				table.insert(self.parts[param5].forbids, param2)
+			end
+		elseif part_type == "forbids_add" then
+			table.insert(self.parts[param2].forbids, param1)
+			if param3 then
+				table.insert(self.parts[param2].forbids, param3)
+			end
+			if param4 then
+				table.insert(self.parts[param2].forbids, param4)
+			end
+			if param5 then
+				table.insert(self.parts[param2].forbids, param5)
+			end
+		elseif part_type == "gadget_rail" then
+			table.insert(self[param1].uses_parts, param2)
+			self[param1].adds[param2] = {param3}
+		elseif part_type == "sight" then
+			if param3 == "specter" then
+				param3 = "wpn_fps_upg_o_specter"
+			end
+			if param3 == "acog" then
+				param3 = "wpn_fps_upg_o_acog"
+			end
+			table.insert(self[param1].uses_parts, param2)
+			self.parts[param2].stance_mod[param1] = deep_clone(self.parts[param3].stance_mod[param4])
+		elseif part_type == "sight_rail" then
+			if param3 == "specter" then
+				param3 = "wpn_fps_upg_o_specter"
+			end
+			if param3 == "acog" then
+				param3 = "wpn_fps_upg_o_acog"
+			end
+			table.insert(self[param1].uses_parts, param2)
+			self[param1].adds[param2] = {param5}
+			self.parts[param2].stance_mod[param1] = deep_clone(self.parts[param3].stance_mod[param4])
+		elseif part_type == "part_a_obj_ovr" then
+			self.parts[param1].override[param2] = {a_obj = param3}
+		elseif part_type == "wpn_a_obj_ovr" then
+			self[param1].override[param2] = {a_obj = param3}
+		elseif part_type == "wpn_a_obj_parent_ovr" then
+			self[param1].override[param2] = {a_obj = param3, parent = param4}
+		elseif part_type == "wpn_parent_ovr" then
+			self[param1].override[param2] = {parent = param3}
+		else
+			table.insert(self[param1].uses_parts, param2)
+		end
+    end
+end
 Hooks:PostHook(WeaponFactoryTweakData, "create_bonuses", "CAFCWMod_MWSNICK_Init", function(self)
+-- Add your mostly used nick on Modworkshop to unique hook id (Change only MWSNICK part).
 
--- Example of adding custom weapon support:
+-- Example of adding custom weapon and custom attachments code:
 if self.wpn_fps_smg_example then
--- 		wpn_fps_smg_example = Factory ID of custom weaon
-if self.parts.wpn_fps_upg_m4_s_caastock and self.parts.wpn_fps_upg_m4_s_caastock_black and self.parts.wpn_fps_upg_m4_s_hke1 and self.parts.wpn_fps_upg_m4_s_prs then
-table.insert(self.wpn_fps_smg_example.uses_parts, "wpn_fps_upg_m4_s_caastock")
-table.insert(self.wpn_fps_smg_example.uses_parts, "wpn_fps_upg_m4_s_caastock_black")
-table.insert(self.wpn_fps_smg_example.uses_parts, "wpn_fps_upg_m4_s_hke1")
-table.insert(self.wpn_fps_smg_example.uses_parts, "wpn_fps_upg_m4_s_prs")
-end
---		Example of most basic attachment type.
-if self.parts.wpn_fps_upg_o_kobra then
-table.insert(self.wpn_fps_smg_example.uses_parts, "wpn_fps_upg_o_kobra")
-	self.parts.wpn_fps_upg_o_kobra.stance_mod.wpn_fps_smg_example = deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_smg_olympic)
-end
---		Custom sight example:
--- 		wpn_fps_upg_o_kobra - ID of custom attachment that custom weapon will support.
--- 		deep_clone(self.parts.wpn_fps_upg_o_specter.stance_mod.wpn_fps_smg_olympic) - Code that copy stance_mod data from wpn_fps_smg_olympic.
-if self.parts.wpn_fps_upg_fl_ass_spotter then
-table.insert(self.wpn_fps_smg_example.uses_parts, "wpn_fps_upg_fl_ass_spotter")
-	self.wpn_fps_smg_example.adds.wpn_fps_upg_fl_ass_spotter = {
-		"wpn_fps_smg_example_gadget_rail"
-	}
-end
---		Example of custom attachment that adds rails/etc.
---		wpn_fps_smg_example_gadget_rail - ID of attachment that will be added along with custom attachment.
+-- This checks if custom weapon is installed.
+
+
+
+
+
+
+
+
 end
 end)
 --[[
-To save time you can always reuse attachment codes from other scripts but remember to replace every Factory ID and double check changes and deep_clone values.
-
 After finishing script for custom weapon save this file as weaponfactorytweakdata_MWSNICK where MWSNICK is your mostly used nick on Modworkshop.
 Open main.xml and add:
 		<hook file="weaponfactorytweakdata_MWSNICK.lua" source_file="lib/tweak_data/weaponfactorytweakdata"/>
